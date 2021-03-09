40756fa98ef980a077dc32d5ac34ce77cf48cbef
Rohan Gupta <rohaninjmu@gmail.com>
2021-03-09 20:57:37 +0530

    Update Taggy.py

--- Desktop/bb/Taggy.py
+++ Desktop/bb/Taggy.py
@@ -10,11 +10,19 @@ chatbot = ChatBot(
     'TestBot',
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
     logic_adapters=[
-        'chatterbot.logic.BestMatch',
-	'chatterbot.logic.TimeLogicAdapter',
-	'chatterbot.logic.MathematicalEvaluation',
-        'default_response': 'I\'m sorry, but I don\'t understand. I am still learning.',
-        'maximum_similarity_threshold': 0.90
+	{
+		'import_path': 'chatterbot.logic.BestMatch',
+		'default_response': 'I\'m sorry, but I don\'t understand. I am still learning.',
+		'maximum_similarity_threshold': 0.90
+	},
+	    
+	{
+		'import_path': 'chatterbot.logic.TimeLogicAdapter',
+	},
+	    
+	{
+		'import_path': 'chatterbot.logic.MathematicalEvaluation'
+	}				
     ],
     database_uri='sqlite:///database.sqlite3'
 ) 
